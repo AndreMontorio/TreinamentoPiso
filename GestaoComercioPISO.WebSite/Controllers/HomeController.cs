a2274abfb81660d34e54094d11f3422ea31d98f6
Andre <andre.montorio@gmail.com>
2017-07-16 12:30:41 -0300

    primeiro commit

---
+++ GestaoComercioPISO.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestaoComercioPISO.WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /*
+        // Editado
+        [Route("~/redirecionar")]
+        public ActionResult Redirecionar()
+        {
+            return RedirectToAction("Index");
+        }
+
+        //Partial
+        [Route("~/partial")]
+        public ActionResult Partial()
+        {
+            return PartialView("_LoginPartial");
+        }
+        */
+
+        [Route("~/sobre")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Essa é a descrição da minha aplicação";
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("~/contato")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("~/contato")]
+        public ActionResult Contact(FormCollection model)
+        {
+            return View();
+        }
+    }
+}
